75ddb888caa2af0bde22188a44cbd57f502d1988
Zhang Tingkuo <zhangtingkuo@gmail.com>
2014-08-16 18:13:49 +0800

    Add MainActivity Menu

--- src/com/arlen/cnblogs/MainActivity.java
+++ src/com/arlen/cnblogs/MainActivity.java
@@ -1,34 +1,115 @@
 package com.arlen.cnblogs;
 
-import android.app.Activity;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.FragmentActivity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewConfiguration;
+import android.view.Window;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+
+	private static boolean isExit;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		setOverflowShowAlways();
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * 两次返回退出程序
+	 */
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			exit();
+			return false;
+		} else {
+			return super.onKeyDown(keyCode, event);
+		}
+	}
+
+	public void exit() {
+		if (!isExit) {
+			isExit = true;
+			Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+			new ExitHandler().sendEmptyMessageDelayed(0, 1000 * 2);
+		} else {
+			Intent intent = new Intent(Intent.ACTION_MAIN);
+			intent.addCategory(Intent.CATEGORY_HOME);
+			startActivity(intent);
+			System.exit(0);
+		}
+	}
+
+	private static class ExitHandler extends Handler {
+
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			isExit = false;
+		}
+	}
+
+	/**
+	 * 统一OverFlow样式
+	 */
+	private void setOverflowShowAlways() {
+		try {
+			ViewConfiguration viewConfiguration = ViewConfiguration.get(this);
+			Field field = ViewConfiguration.class
+					.getDeclaredField("sHasPermanentMenuKey");
+			field.setAccessible(true);
+			field.setBoolean(viewConfiguration, false);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * OverFlow显示图标
+	 */
+	@Override
+	public boolean onMenuOpened(int featureId, Menu menu) {
+		if (featureId == Window.FEATURE_ACTION_BAR && menu != null) {
+			if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+				try {
+					Method method = menu.getClass().getDeclaredMethod(
+							"setOptionalIconsVisible", Boolean.TYPE);
+					method.setAccessible(true);
+					method.invoke(menu, true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return super.onMenuOpened(featureId, menu);
 	}
 }
