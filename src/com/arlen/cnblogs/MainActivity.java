16b85a3fc17842f14e3ab8b86b21e1ba1092a21c
Zhang Tingkuo <zhangtingkuo@gmail.com>
2014-08-17 16:40:55 +0800

    Add Icon for every item of DrawerList (reset list adapter)

--- src/com/arlen/cnblogs/MainActivity.java
+++ src/com/arlen/cnblogs/MainActivity.java
@@ -3,11 +3,10 @@ package com.arlen.cnblogs;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
-import com.arlen.cnblogs.fragment.BlogFragment;
-
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -17,16 +16,22 @@ import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.arlen.cnblogs.fragment.BlogFragment;
 
 public class MainActivity extends Activity {
 	private DrawerLayout drawerLayout;
@@ -36,6 +41,8 @@ public class MainActivity extends Activity {
 	private CharSequence drawerTitle;
 	private CharSequence actionBarTitle;
 	private String[] drawerListItem;
+	private int[] drawerListImage = { R.drawable.ic_drawer_list_blog,
+			R.drawable.ic_drawer_list_news, R.drawable.ic_drawer_list_user };
 
 	private static boolean isExit;
 
@@ -43,8 +50,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		//unit menu style
+
+		// unit menu style
 		setOverflowShowAlways();
 
 		actionBarTitle = drawerTitle = getTitle();
@@ -55,8 +62,9 @@ public class MainActivity extends Activity {
 
 		drawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
 				GravityCompat.START);
-		drawerList.setAdapter(new ArrayAdapter<String>(this,
-				R.layout.drawer_list_item, drawerListItem));
+
+		drawerList.setAdapter(new DrawerListAdapter(getApplicationContext(),
+				drawerListItem, drawerListImage));
 		drawerList.setOnItemClickListener(new DrawerItemClickListener());
 
 		getActionBar().setDisplayHomeAsUpEnabled(true);
@@ -111,6 +119,60 @@ public class MainActivity extends Activity {
 		default:
 			return super.onOptionsItemSelected(item);
 		}
+	}
+
+	private class DrawerListAdapter extends BaseAdapter {
+		private Context context;
+		private String[] contenct;
+		private int[] images;
+
+		public DrawerListAdapter(Context context, String[] contenct,
+				int[] images) {
+			super();
+			this.context = context;
+			this.contenct = contenct;
+			this.images = images;
+		}
+
+		@Override
+		public int getCount() {
+			return contenct.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if (convertView == null) {
+				convertView = LayoutInflater.from(context).inflate(
+						R.layout.drawer_list_item, null);
+				ItemViewCache itemViewCache = new ItemViewCache();
+				itemViewCache.imageView = (ImageView) convertView
+						.findViewById(R.id.imageViewIcon);
+				itemViewCache.textView = (TextView) convertView
+						.findViewById(R.id.textViewTitle);
+				convertView.setTag(itemViewCache);
+			}
+
+			ItemViewCache chche = (ItemViewCache) convertView.getTag();
+			chche.imageView.setImageResource(images[position]);
+			chche.textView.setText(contenct[position]);
+			return convertView;
+		}
+
+		private class ItemViewCache {
+			public ImageView imageView;
+			public TextView textView;
+		}
+
 	}
 
 	private class DrawerItemClickListener implements
